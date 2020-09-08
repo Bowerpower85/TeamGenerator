0d69ae25128ae610b646fa96218e8e2e11848e18
Jonathan Bower <bowerpower85@gmail.com>
2020-09-07 17:14:08 -0700

    Added the final functionality to my app.js then had to troubleshoot some syntax issues to make it run correctly.

--- app.js
+++ app.js
@@ -4,35 +4,16 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const render = require("./lib/htmlRenderer");
+const { get } = require("https");
+let employee = [];
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
 
 async function generateTeam() {
     await getManagerInput();
@@ -61,7 +42,7 @@ function teamQuestions() {
 
 };
 
-function getManagerOutput() {
+function getManagerInput() {
     const questions = [
         {
             type: 'input',
@@ -88,7 +69,7 @@ function getManagerOutput() {
     inquirer.prompt(questions)
         .then(answer => {
             let newManager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
-            employees.push(newManager);
+            employee.push(newManager);
             teamQuestions();
         });
 };
@@ -126,11 +107,11 @@ function getEngineerInput() {
         inquirer.prompt(questions)
             .then(answer => {
                 let newEngineer = new Engineer(answer.name, answer.id, answer.email, answer.github);
-                employees.push(newEngineer);
+                employee.push(newEngineer);
                 if(answer.askAgain) {
                     teamQuestions();
                 } else {
-                    let HTML = render(employees);
+                    let HTML = render(employee);
                     writeFileAsync(outputPath, HTML);
                     return
                 }
@@ -138,3 +119,51 @@ function getEngineerInput() {
     }
     ask();
 };
+
+function getInternInput() {
+    const questions = [
+        {
+            type: 'input',
+            message: "What is the Intern's name?",
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: "What is the Intern's id?",
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: "What is the Intern's email?",
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: "What is the Intern's school?",
+            name: 'school'
+        },
+        {
+            type: 'confirm',
+            message: "Would you like to add another employee?",
+            name: 'askAgain'
+        },
+    ]
+
+    function ask() {
+        inquirer.prompt(questions)
+            .then(answer => {
+                let newIntern = new Intern(answer.name, answer.id, answer.email, answer.school);
+                employee.push(newIntern);
+                if (answer.askAgain) {
+                    teamQuestions();
+                } else {
+                    let HTML = render(employee);
+                    writeFileAsync(outputPath, HTML);
+                    return
+                }
+            });
+    }
+    ask();
+};
+
+generateTeam();
